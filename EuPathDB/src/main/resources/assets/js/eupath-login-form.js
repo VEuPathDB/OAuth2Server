120da528af54ef0ba48505d81806b20877390a53
Ryan Doherty <tech@conical.org>
2016-02-25 04:49:26 +0000

    Fix cancel button and provide project-specific logo based on which api site user came from
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/OAuth2Server/trunk@17882 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- EuPathDB/src/main/resources/assets/js/eupath-login-form.js
+++ EuPathDB/src/main/resources/assets/js/eupath-login-form.js
@@ -4,17 +4,27 @@ $(function() {
   var redirectUri = getUrlParams()["redirectUri"];
   if (redirectUri == '') {
     var baseUrl = 'http://eupathdb.org/eupathdb';
+    var cancelUrl = baseUrl;
+    var project = 'eupathdb';
   }
   else {
     var url = parseURL(redirectUri);
     var webappName = getWebappName(url.pathname);
+    var hostParts = url.host.split('.');
     var baseUrl = url.protocol + "//" + url.host + "/" + webappName;
+    var cancelUrl = (url.searchObject.redirectUrl == undefined ?
+        baseUrl : decodeURIComponent(url.searchObject.redirectUrl));
+    var project = hostParts[hostParts.length - 2];
+    if (project == 'globus' || project == 'globusgenomics') project = 'eupathdb';
   }
 
   // add custom URLs to form
   $('#new-account').attr('href', baseUrl + "/showRegister.do");
   $('#forgot-password').attr('href', baseUrl + "/showResetPassword.do");
-  $('.cancel-button').click(function(){ window.location = baseUrl; });
+  $('.cancel-button').click(function(){ window.location = cancelUrl; });
+
+  // choose the correct header logo
+  $('#' + project + '-logo').css({ display: 'inline' });
 
 });
 
