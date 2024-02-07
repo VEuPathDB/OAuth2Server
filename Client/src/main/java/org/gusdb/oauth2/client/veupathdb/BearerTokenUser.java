93547da79dc9764b82a87d7fea09348859bef32d
Ryan Doherty <tech@conical.org>
2024-02-07 14:53:57 -0500

    Move User class and related classes into OAuth so they can be shared with services (previously these were duplicated in both places)

---
+++ Client/src/main/java/org/gusdb/oauth2/client/veupathdb/BearerTokenUser.java
@@ -0,0 +1,51 @@
+package org.gusdb.oauth2.client.veupathdb;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.gusdb.oauth2.client.OAuthClient;
+import org.gusdb.oauth2.client.ValidatedToken;
+import org.gusdb.oauth2.shared.IdTokenFields;
+import org.json.JSONObject;
+
+public class BearerTokenUser extends User {
+
+  private static final Logger LOG = LogManager.getLogger(BearerTokenUser.class);
+
+  private final OAuthClient _client;
+  private final String _oauthUrl;
+  private final ValidatedToken _token;
+  private boolean _userInfoFetched = false;
+
+  public BearerTokenUser(OAuthClient client, String oauthUrl, ValidatedToken token) {
+    // parent constructor sets immutable fields provided on the token
+    super(
+        Long.valueOf(token.getUserId()),
+        token.isGuest(),
+        token.getTokenContents().get(IdTokenFields.signature.name(), String.class),
+        token.getTokenContents().get(IdTokenFields.preferred_username.name(), String.class));
+    _client = client;
+    _oauthUrl = oauthUrl;
+    _token = token;
+  }
+
+  @Override
+  protected void fetchUserInfo() {
+    // return if already fetched
+    if (_userInfoFetched) return;
+
+    LOG.info("User data fetch requested for user " + getUserId() + "; querying OAuth server.");
+    // fetch user info from OAuth server where it is stored (but only on demand, and only once for this object's lifetime)
+    JSONObject userInfo = _client.getUserData(_oauthUrl, _token);
+
+    // set email (standard property but mutable so set on user profile and not token
+    setEmail(userInfo.getString(IdTokenFields.email.name()));
+
+    // set other user properties found only on user profile object
+    setPropertyValues(userInfo);
+
+    LOG.info("User data successfully fetched for " + getDisplayName() + " / " + getOrganization());
+    _userInfoFetched = true;
+  }
+
+}
+
---
+++ Client/src/main/java/org/gusdb/oauth2/client/veupathdb/User.java
@@ -0,0 +1,183 @@
+package org.gusdb.oauth2.client.veupathdb;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.json.JSONObject;
+
+/**
+ * Represents a VEupathDB user
+ *
+ * @author rdoherty
+ */
+public class User {
+
+  public static final Map<String,UserProperty> USER_PROPERTIES = createUserPropertyDefs();
+
+  private static Map<String,UserProperty> createUserPropertyDefs() {
+    List<UserProperty> userProps = List.of(
+        new UserProperty("username", "Username", "username", false, false, false, User::getUsername, User::setUsername),
+        new UserProperty("firstName", "First Name", "first_name", true, true, false, User::getFirstName, User::setFirstName),
+        new UserProperty("middleName", "Middle Name", "middle_name", false, true, false, User::getMiddleName, User::setMiddleName),
+        new UserProperty("lastName", "Last Name", "last_name", true, true, false, User::getLastName, User::setLastName),
+        new UserProperty("organization", "Organization", "organization", true, true, false, User::getOrganization, User::setOrganization),
+        new UserProperty("interests", "Interests", "interests", false, false, true, User::getInterests, User::setInterests)
+    );
+    return Collections.unmodifiableMap(userProps.stream().collect(Collectors.toMap(UserProperty::getName, x -> x)));
+  }
+
+  // immutable fields supplied by bearer token
+  private final long _userId;
+  private final boolean _isGuest;
+  private final String _signature;
+  private final String _stableId;
+
+  // mutable fields that may need to be fetched
+  private String _email; // standard; not a user property
+  private String _username;
+  private String _firstName;
+  private String _middleName;
+  private String _lastName;
+  private String _organization;
+  private String _interests;
+
+  public User(long userId, boolean isGuest, String signature, String stableId) {
+    _userId = userId;
+    _isGuest = isGuest;
+    _signature = signature;
+    _stableId = stableId;
+  }
+
+  public void setPropertyValues(JSONObject json) {
+    for (UserProperty userProp : User.USER_PROPERTIES.values()) {
+      userProp.setValue(this, json.optString(userProp.getName(), null));
+    }
+  }
+
+  public long getUserId() {
+    return _userId;
+  }
+
+  public boolean isGuest() {
+    return _isGuest;
+  }
+
+  public String getSignature() {
+    return _signature;
+  }
+
+  public String getStableId() {
+    return _stableId;
+  }
+
+  protected void fetchUserInfo() {
+    // nothing to do in this base class; all info must be explicitly set
+  }
+
+  public String getEmail() {
+    fetchUserInfo();
+    return _email;
+  }
+
+  public User setEmail(String email) {
+    _email = email;
+    return this;
+  }
+
+  public String getUsername() {
+    fetchUserInfo();
+    return _username;
+  }
+
+  public User setUsername(String username) {
+    _username = username;
+    return this;
+  }
+
+  public String getFirstName() {
+    fetchUserInfo();
+    return _firstName;
+  }
+
+  public User setFirstName(String firstName) {
+    _firstName = firstName;
+    return this;
+  }
+
+  public String getMiddleName() {
+    fetchUserInfo();
+    return _middleName;
+  }
+
+  public User setMiddleName(String middleName) {
+    _middleName = middleName;
+    return this;
+  }
+
+  public String getLastName() {
+    fetchUserInfo();
+    return _lastName;
+  }
+
+  public User setLastName(String lastName) {
+    _lastName = lastName;
+    return this;
+  }
+
+  public String getOrganization() {
+    fetchUserInfo();
+    return _organization;
+  }
+
+  public User setOrganization(String organization) {
+    _organization = organization;
+    return this;
+  }
+
+  public String getInterests() {
+    fetchUserInfo();
+    return _interests;
+  }
+
+  public User setInterests(String interests) {
+    _interests = interests;
+    return this;
+  }
+
+  /**
+   * Provides a "pretty" display name for this user
+   * 
+   * @return display name for this user
+   */
+  public String getDisplayName() {
+    return isGuest() ? "Guest User" : (
+        formatNamePart(getFirstName()) +
+        formatNamePart(getMiddleName()) +
+        formatNamePart(getLastName())).trim();
+  }
+
+  private static String formatNamePart(String namePart) {
+    return (namePart == null || namePart.isEmpty() ? "" : " " + namePart.trim());
+  }
+
+  @Override
+  public String toString() {
+    return "User #" + getUserId() + " - " + getEmail();
+  }
+
+  @Override
+  public int hashCode() {
+    return String.valueOf(getUserId()).hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof User)) {
+      return false;
+    }
+    return getUserId() == ((User)obj).getUserId();
+  }
+
+}
