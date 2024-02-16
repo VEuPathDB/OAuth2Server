690ddfa846570d650a450a2f16574d3e73b40437
Ryan Doherty <tech@conical.org>
2024-02-16 16:17:39 -0500

    Update to become a real unit test

--- Server/src/test/java/org/gusdb/oauth2/service/ListEnumerationTest.java
+++ Server/src/test/java/org/gusdb/oauth2/service/ListEnumerationTest.java
@@ -5,6 +5,7 @@ import java.util.Enumeration;
 import java.util.List;
 
 import org.gusdb.oauth2.service.util.JerseyHttpRequestWrapper.ListEnumeration;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ListEnumerationTest {
@@ -13,8 +14,11 @@ public class ListEnumerationTest {
   public void testEnumeration() {
     List<String> items = Arrays.asList(new String[]{ "one", "two", "three" });
     Enumeration<String> enumer = new ListEnumeration<>(items);
+    int i = 0;
     while (enumer.hasMoreElements()) {
-      System.out.println(enumer.nextElement());
+      Assert.assertEquals(items.get(i), enumer.nextElement());
+      i++;
     }
+    Assert.assertEquals(items.size(), i);
   }
 }
