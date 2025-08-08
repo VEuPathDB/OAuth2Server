a351f7d0d8730f0d46efe2c47d7f84f24e3c5229
Ryan Doherty <tech@conical.org>
2025-08-08 16:59:26 -0400

    Add lookup SQL, fill in groups endpoint, improve group_type display names

--- EuPathDB/src/main/java/org/gusdb/oauth2/eupathdb/subscriptions/SubscriptionService.java
+++ EuPathDB/src/main/java/org/gusdb/oauth2/eupathdb/subscriptions/SubscriptionService.java
@@ -1,56 +1,87 @@
 package org.gusdb.oauth2.eupathdb.subscriptions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
-import org.gusdb.oauth2.config.ApplicationConfig;
+import org.gusdb.fgputil.IoUtil;
+import org.gusdb.fgputil.db.runner.SQLRunner;
 import org.gusdb.oauth2.eupathdb.AccountDbAuthenticator;
 import org.gusdb.oauth2.server.OAuthServlet;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class SubscriptionService {
 
   @Context
   private ServletContext _context;
 
-  @Context
-  private HttpServletRequest _request;
-
-  @Context
-  private HttpHeaders _headers;
-
-  private ApplicationConfig getApplicationConfig() {
-    return OAuthServlet.getApplicationConfig(_context);
+  private DataSource getAcctDbDs() {
+    return ((AccountDbAuthenticator) OAuthServlet.getAuthenticator(_context))
+        .getAccountDb().getDataSource();
   }
 
-  private AccountDbAuthenticator getAuthenticator() {
-    return (AccountDbAuthenticator) OAuthServlet.getAuthenticator(_context);
-  }
+  private static final String GROUP_LEADS_SQL_PATH = "sql/select-group-leads.sql";
+  private static final String GROUP_LEADS_SQL = getGroupLeadsSql();
 
-  // returns list of subscriptions
+  // returns list of subscribed groups
   @GET
-  @Path("/subscriptions")
-  public Response getSubscriptions() {
-    return Response.ok().build();
+  @Path("/groups")
+  public Response getSubscribedGroups() {
+    // TODO: break DB work out of the service class; ok here for now
+    return Response.ok(new SQLRunner(getAcctDbDs(), GROUP_LEADS_SQL).executeQuery(rs -> {
+      Map<String, JSONObject> groups = new HashMap<>(); // keyed on subscription token
+
+      // each row represents a group + group lead (group lead may be null if group has no leads)
+      while (rs.next()) {
+
+        // parse columns
+        String subscriptionToken = rs.getString("subscription_token");
+        String groupName = rs.getString("group_name");
+        String leadFirstName = rs.getString("first_name");
+        String leadLastName = rs.getString("last_name");
+        String leadOrganization = rs.getString("organization");
+
+        // see if group already exists and create new one if not
+        JSONObject group = groups.get(subscriptionToken);
+        if (group == null) {
+          group = new JSONObject()
+              .put("subscriptionToken", subscriptionToken)
+              .put("groupName", groupName)
+              .put("groupLeads", new JSONArray());
+          groups.put(subscriptionToken, group);
+        }
+
+        // add lead to this group if lead is present
+        if (leadFirstName != null) {
+          group.getJSONArray("groupLeads").put(new JSONObject()
+              .put("name",leadFirstName + " " + leadLastName)
+              .put("organization", leadOrganization));
+        }
+      }
+      return new JSONArray(groups.values());
+    }).toString(2)).build();
   }
 
-  // returns info about a single subscription including groups/leads/members
-  @GET
-  @Path("/subscriptions/{subscriptionId}")
-  public Response getSubscription(@QueryParam("subscriptionId") int subscriptionId) {
-    return Response.ok().build();
-  }
-
-  // returns known groups that are not subscribed
-  @GET
-  @Path("/groups/orphan")
-  public Response getOrphanGroups() {
-    return Response.ok().build();
+  private static String getGroupLeadsSql() {
+    URL url = Thread.currentThread().getContextClassLoader().getResource(GROUP_LEADS_SQL_PATH);
+    if (url == null) throw new RuntimeException("Unable to read resource " + GROUP_LEADS_SQL_PATH);
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+      return IoUtil.readAllChars(reader);
+    }
+    catch (IOException e) {
+      throw new RuntimeException("Unable to read resource " + GROUP_LEADS_SQL_PATH, e);
+    }
   }
 
 }
---
+++ EuPathDB/src/main/resources/sql/select-group-leads.sql
@@ -0,0 +1,22 @@
+select g.subscription_token, g.group_name, p.first_name, p.last_name, p.organization
+from (
+    select l.user_id, s.*
+    from (
+      select g.subscription_token, g.group_name, g.group_id
+      from useraccounts.subscription_groups g, useraccounts.subscriptions s
+      where g.subscription_id = s.subscription_id
+      and s.is_active = 1
+    ) s
+    left join useraccounts.subscription_group_leads l
+    on l.group_id = s.group_id
+) g
+left join (
+    select
+      user_id,
+      max(case when key = 'first_name' then value end) as first_name,
+      max(case when key = 'last_name' then value end) as last_name,
+      max(case when key = 'organization' then value end) as organization
+    from useraccounts.account_properties
+    group by user_id
+) p
+on g.user_id = p.user_id
